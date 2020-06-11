a267000aeebb672893883daf24b9fb619dabd2bd
Nicaodev <alexandre.nicos@gmail.com>
2020-06-11 01:06:46 -0300

    feature/1.0 - basic refactoring

---
+++ WebApi-full-stack/WebApi-full-stack/Controllers/EventoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using WebApi_full_stack.Model;
+
+namespace WebApi_full_stack.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController(ILogger<EventoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return new Evento[]
+            {
+                new Evento()
+                {
+                Id = Guid.NewGuid(),
+                CargaHoraria=20,
+                Cidade = "Valparaíso de Goiás",
+                Data = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy"),
+                Nome = "Curso de Angular"
+                }
+            };
+        }
+    }
+}
