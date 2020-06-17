86ee958222efc8e3955a00980991095dc2151c6e
Nicaodev <alexandre.nicos@gmail.com>
2020-06-16 23:46:35 -0300

    feature/2.0.0 - Helpers and Pipe

--- WebApi-app/src/app/app.module.ts
+++ WebApi-app/src/app/app.module.ts
@@ -2,25 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { DateTimeFormatPipePipe } from './helpers/dateTimeFormatPipe.pipe';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+import { EventoService } from './services/evento.service';
+
 
 @NgModule({
    declarations: [
       AppComponent,
       EventosComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipePipe // Pipes personalizado
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot(),
       HttpClientModule, // para requisições na webApi,
       FormsModule
    ],
-   providers: [],
+   providers: [
+     EventoService
+   ],
    bootstrap: [
       AppComponent
    ]
