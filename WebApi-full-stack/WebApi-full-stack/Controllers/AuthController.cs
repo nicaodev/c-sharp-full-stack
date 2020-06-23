58fd40037502c8f5632c6ed8ef6cb1a1aea1e5cf
Nicaodev <alexandre.nicos@gmail.com>
2020-06-22 23:03:55 -0300

    feature/3.1.0 - Config JWT, requisitos Identity e AuthController.

---
+++ WebApi-full-stack/WebApi-full-stack/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApi.Domain.Identity;
+using WebApi_full_stack.Dtos;
+
+namespace WebApi_full_stack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+
+        public AuthController(IConfiguration config, UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        {
+            _config = config;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet("GetUser")]
+        public async Task<IActionResult> GetUser(UserDto userDto)
+        {
+            return Ok(userDto);
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous] // Não precisa autenticar pq aqui que iremos criar um user.
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            try
+            {
+                var user = _mapper.Map<User>(userDto);
+
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+
+                var retorno = _mapper.Map<UserDto>(user);
+
+                if (result.Succeeded)
+                    return Created("GetUser", retorno);
+
+                return BadRequest(result.Errors);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco de dados falhou: {ex.Message}");
+            }
+        }
+
+
+    }
+}
